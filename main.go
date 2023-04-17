724a5e07acbdf73fe5e6120516591cae5a04d8e1
Darren Kellenschwiler <d.kellenschwiler@bitcoinassociation.net>
2023-04-16 21:28:32 -0500

    simpler

--- main.go
+++ main.go
@@ -19,11 +19,7 @@ func main() {
 			Nug: "hello",
 			Gus: 1234,
 		}
-		res, err := json.Marshall(data)
-		if err != nil {
-			return e.Logger.Fatal(err)
-		}
-		return c.String(http.StatusOK, res)
+		return c.JSON(http.StatusOK, data)
 	})
 	e.Logger.Fatal(e.Start(":80"))
 }
