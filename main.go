8173a6ad7c953f4ec901a49dbbb0749b07eaf542
Darren Kellenschwiler <d.kellenschwiler@bitcoinassociation.net>
2023-04-17 21:24:17 -0500

    account

--- main.go
+++ main.go
@@ -6,19 +6,34 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-type JSONresponse struct {
-	Nug string
-	Gus int 
+type Account struct {
+	name string
+}
+
+type HomeResponse struct {
+	Operational bool
+}
+
+type AccountsResponse struct {
+	Accounts []*Account
+}
+
+func home(c echo.Context) error {
+	data := &HomeResponse{
+		Operational: true,
+	}
+	return c.JSON(http.StatusOK, data)
+}
+
+func accounts(c echo.Context) error {
+	data := make(AccountsResponse, 0)
+	data = data.append(&Account{"deggen"})
+	return c.JSON(http.StatusOK, data)
 }
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		data := &JSONresponse{
-			Nug: "hello",
-			Gus: 1234,
-		}
-		return c.JSON(http.StatusOK, data)
-	})
+	e.GET("/", home)
+	e.GET("/accounts", accounts)
 	e.Logger.Fatal(e.Start(":3000"))
 }
